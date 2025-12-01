2fea9366f3d383213cd65ae65592aabd211f10fe
TDC <trevordcooley@gmail.com>
2025-12-01 16:24:56 -0500

    Transform rps console app to web page gui app

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,3 @@
-
 function getComputerChoice()
 {
     const randNum = Math.floor(Math.random() * 3);
@@ -6,68 +5,93 @@ function getComputerChoice()
     return randNum === 0 ? "rock" : randNum === 1 ? "paper" : "scissors";
 }
 
-function getHumanChoice()
-{
-    return prompt("Choose rock, paper, or scissors by typing it: ");
-}
-
-
-
 function playGame()
 {
     let humanScore = 0;
     let computerScore = 0;
+    let numRounds = 0;
 
     function playRound(humanChoice, computerChoice)
     {
         const userChoice = humanChoice.toLowerCase();
+
+        const roundWinnerDisplay = document.querySelector(".round-winner");
+
+        const computerChoiceDisplay = document.querySelector(".computer-choice");
+        const userChoiceDisplay = document.querySelector(".user-choice");
         
-        console.log(`Computer's choice: ${computerChoice}`);
-        console.log(`Your choice: ${userChoice}`);
+        computerChoiceDisplay.textContent = `Computer's choice: ${computerChoice}`;
+        userChoiceDisplay.textContent = `Your choice: ${userChoice}`;
 
         if(userChoice === computerChoice)
         {
-            console.log(`Draw! ${userChoice} is equal to ${computerChoice}.`);
+            roundWinnerDisplay.textContent = `Draw! ${userChoice} is equal to ${computerChoice}.`;
         }
         else if((userChoice === "rock" && computerChoice === "scissors") ||
             (userChoice === "paper" && computerChoice === "rock") ||
             (userChoice === "scissors" && computerChoice === "paper"))
         {
             humanScore++;
-            console.log(`You win! ${userChoice} beats ${computerChoice}.`);
+            roundWinnerDisplay.textContent = `You win! ${userChoice} beats ${computerChoice}.`;
         }
         else
         {
             computerScore++;
-            console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+            roundWinnerDisplay.textContent = `You lose! ${computerChoice} beats ${userChoice}.`;
+        }
+    }
+
+    function displayScore(humanScore, computerScore)
+    {
+        const humanScoreDisplay = document.querySelector(".human-score")
+        const computerScoreDisplay = document.querySelector(".computer-score")
+
+        humanScoreDisplay.textContent = `${humanScore}`;
+        computerScoreDisplay.textContent = `${computerScore}`;
+    }
+
+    function displayWinner(humanScore, computerScore)
+    {
+        const winnerDisplay = document.querySelector(".winner");
+        const humanFinalPoints = document.querySelector(".human-final-points");
+        const computerFinalPoints = document.querySelector(".computer-final-points");
+        
+        humanFinalPoints.textContent = `Your final points: ${humanScore}`;
+        computerFinalPoints.textContent = `Computer's final points: ${computerScore}`;
+                
+        if(humanScore > computerScore)
+        {
+            winnerDisplay.textContent = `You are the ultimate winner with ${humanScore} points`;
+        }
+        else if(computerScore > humanScore)
+        { 
+            winnerDisplay.textContent = `The computer is the ultimate winner with ${computerScore} points`;
+        }
+        else 
+        {
+            winnerDisplay.textContent = `You both had ${computerScore} point(s). It's a draw!`;
         }
     }
     
-    for(let i = 0; i < 5; i++)
-    {
-        const humanChoice = getHumanChoice();
+    const rpsButtons = document.querySelectorAll(".rps-buttons button");
+
+    rpsButtons.forEach((item) => {
+        item.addEventListener("click", (e) => {
+        numRounds++;
         const computerChoice = getComputerChoice();
+        playRound(e.target.innerText, computerChoice);
+        displayScore(humanScore, computerScore);
+        if(numRounds === 5)
+        {
+            displayWinner(humanScore, computerScore);
+        }
+        });
 
-        playRound(humanChoice, computerChoice);
-        console.log(`Human score: ${humanScore}`);
-        console.log(`Computer score: ${computerScore}`);
-    }
+    });
 
-    console.log(`Your final points: ${humanScore}`);
-    console.log(`Computer's final points: ${computerScore}`);
-    
-    if(humanScore > computerScore)
-    {
-        console.log(`You are the ultimate winner with ${humanScore} points`);
-    }
-    else if(computerScore > humanScore)
-    { 
-        console.log(`The computer is the ultimate winner with ${computerScore} points`);
-    }
-    else 
-    {
-        console.log(`You both had ${computerScore} point(s). It's a draw!`);
-    }
+
 }
 
-//playGame();
+playGame();
+
+
